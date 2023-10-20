594fef9420d4c0bc80bc03388eeb80cc68e2e7db
ananth488 <ananthjacks28@gmail.com>
2023-10-20 14:54:20 +0530

    ddsd

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,52 +1,110 @@
-import React from 'react'
-import styles from './Home.module.css'
+import React, { useState } from "react";
+import styles from "./Home.module.css";
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
 
 const Home = () => {
-    return (
-        <>
-        <div className={styles.homepage_main}>
-            {/* bannerimag */}
-            <div className={styles.bannerimg_main}>
-                <div className={styles.bannerimg_main_first}>
-                    <h1>Campus Management Solutions For <br />Educational Institutions</h1>
-                    <p>Distinct product offerings for Schools, Colleges, Universities & Trusts.</p>
-                    <p>Automation of Students, Faculty & Campus Administration.</p>
-                    <p>Learning Outcome Management & Analysis to promote Student Success</p>
-                    <p>Automation of Students, Faculty & Campus Administration.</p>
-                    <p>Learning Outcome Management & Analysis to promote Student Success</p>
-                </div>
-            </div>
+  const [counteron, setCounterOn] = useState(false);
+  return (
+    <>
+      <div className={styles.homepage_main}>
+        {/* bannerimag */}
+        <div className={styles.bannerimg_main}>
+          <div className={styles.bannerimg_main_first}>
+            <h1>
+              Campus Management Solutions For <br />
+              Educational Institutions
+            </h1>
+            <p>
+              Distinct product offerings for Schools, Colleges, Universities &
+              Trusts.
+            </p>
+            <p>Automation of Students, Faculty & Campus Administration.</p>
+            <p>
+              Learning Outcome Management & Analysis to promote Student Success
+            </p>
+            <p>Automation of Students, Faculty & Campus Administration.</p>
+            <p>
+              Learning Outcome Management & Analysis to promote Student Success
+            </p>
+          </div>
         </div>
-        {/* discovery */}
-        <div className={styles.discovery_main}>
-            <div className={styles.discovery_main_first}>
-              <h1>DISCOVER</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto aperiam unde natus voluptates placeat accusamus vel laborum cupiditate. Reiciendis commodi perferendis dignissimos, amet quis.
-              </p>
-            </div>
-            <div className={styles.discovery_main_second}>
-              <div className={styles.discovery_main_second_first}>
-                 <img src='disc 1.webp' alt='correct'></img>
-              </div>
-              <div className={styles.discovery_main_second_second}>
-                 <h1>BUILD SCHOOLS IN CHENNAI</h1>
-                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam quis, nemo explicabo cupiditate vero fugiat sit eius sequi.</p>
-              </div>
-            </div>
-            {/*  */}
-        
-            <div className={styles.discovery_main_second}>
-              <div className={styles.discovery_main_second_first}>
-                 <h1>BUILD SCHOOLS IN CHENNAI</h1>
-                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam quis, nemo explicabo cupiditate vero fugiat sit eius sequi.</p>
-              </div>
-              <div className={styles.discovery_main_second_second}>
-              <img src='disc 2.webp' alt='correct'></img>
-              </div>
-            </div>
+      </div>
+      {/* discovery */}
+      <div className={styles.discovery_main}>
+        <div className={styles.discovery_main_first}>
+          <h1>DISCOVER</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto
+            aperiam unde natus voluptates placeat accusamus vel laborum
+            cupiditate. Reiciendis commodi perferendis dignissimos, amet quis.
+          </p>
         </div>
-        </>
-    )
-}
+        <div className={styles.discovery_main_second}>
+          <div className={styles.discovery_main_second_first}>
+            <img src="disc 1.webp" alt="correct"></img>
+          </div>
+          <div className={styles.discovery_main_second_second}>
+            <h1>BUILD SCHOOLS IN CHENNAI</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+              quis, nemo explicabo cupiditate vero fugiat sit eius sequi.
+            </p>
+            <div className={styles.discovery_main_second_third}>
+              <div className={styles.discovery_main_second_third_first}>
+                <ScrollTrigger onEnter={() => setCounterOn(true)}onExit={() => setCounterOn(false)}>
+                  <h1>
+                    {counteron && (<CountUp start={0} end={15} duration={2} delay={0} />)}
+                  </h1>
+                </ScrollTrigger>
+                <h5>Schools</h5>
+              </div>
+              <div className={styles.discovery_main_second_third_second}>
+                <ScrollTrigger onEnter={() => setCounterOn(true)} onExit={() => setCounterOn(false)}>
+                  <h1>
+                    {counteron && (<CountUp start={0} end={1450} duration={2} delay={0} /> )}
+                  </h1>
+                </ScrollTrigger>
+                <h5>Students</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/*  */}
 
-export default Home
+        <div className={styles.discovery_main_second}>
+          <div className={styles.discovery_main_second_second}>
+            <h1>BUILD SCHOOLS IN CHENNAI</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+              quis, nemo explicabo cupiditate vero fugiat sit eius sequi.
+            </p>
+            <div className={styles.discovery_main_second_third}>
+              <div className={styles.discovery_main_second_third_first}>
+                <ScrollTrigger onEnter={() => setCounterOn(true)}onExit={() => setCounterOn(false)}>
+                  <h1>
+                    {counteron && (<CountUp start={0} end={4560} duration={2} delay={0} />)}
+                  </h1>
+                </ScrollTrigger>
+                <h5>Children</h5>
+              </div>
+              <div className={styles.discovery_main_second_third_second}>
+                <ScrollTrigger onEnter={() => setCounterOn(true)} onExit={() => setCounterOn(false)}>
+                  <h1>
+                    {counteron && (<CountUp start={0} end={38} duration={2} delay={0} /> )}
+                  </h1>
+                </ScrollTrigger>
+                <h5>Orphanage</h5>
+              </div>
+            </div>
+          </div>
+          <div className={styles.discovery_main_second_first}>
+            <img src="disc 2.webp" alt="correct"></img>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
