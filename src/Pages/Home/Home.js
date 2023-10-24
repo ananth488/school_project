dd8757182024ac36f74b06162612f44a844e7d74
ananth488 <ananthjacks28@gmail.com>
2023-10-24 14:44:53 +0530

    dvvf

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,6 +3,7 @@ import styles from "./Home.module.css";
 import CountUp from "react-countup";
 import ScrollTrigger from "react-scroll-trigger";
 
+
 const Home = () => {
   const [counteron, setCounterOn] = useState(false);
   return (
@@ -55,17 +56,27 @@ const Home = () => {
             </p>
             <div className={styles.discovery_main_second_third}>
               <div className={styles.discovery_main_second_third_first}>
-                <ScrollTrigger onEnter={() => setCounterOn(true)}onExit={() => setCounterOn(false)}>
+                <ScrollTrigger
+                  onEnter={() => setCounterOn(true)}
+                  onExit={() => setCounterOn(false)}
+                >
                   <span>
-                    {counteron && (<CountUp start={0} end={15} duration={2} delay={0} />)}
+                    {counteron && (
+                      <CountUp start={0} end={15} duration={2} delay={0} />
+                    )}
                   </span>
                 </ScrollTrigger>
                 <h5>Schools</h5>
               </div>
               <div className={styles.discovery_main_second_third_second}>
-                <ScrollTrigger onEnter={() => setCounterOn(true)} onExit={() => setCounterOn(false)}>
+                <ScrollTrigger
+                  onEnter={() => setCounterOn(true)}
+                  onExit={() => setCounterOn(false)}
+                >
                   <span>
-                    {counteron && (<CountUp start={0} end={1450} duration={2} delay={0} /> )}
+                    {counteron && (
+                      <CountUp start={0} end={1450} duration={2} delay={0} />
+                    )}
                   </span>
                 </ScrollTrigger>
                 <h5>Students</h5>
@@ -84,17 +95,27 @@ const Home = () => {
             </p>
             <div className={styles.discovery_main_second_third}>
               <div className={styles.discovery_main_second_third_first}>
-                <ScrollTrigger onEnter={() => setCounterOn(true)}onExit={() => setCounterOn(false)}>
+                <ScrollTrigger
+                  onEnter={() => setCounterOn(true)}
+                  onExit={() => setCounterOn(false)}
+                >
                   <span>
-                    {counteron && (<CountUp start={0} end={4560} duration={2} delay={0} />)}
+                    {counteron && (
+                      <CountUp start={0} end={4560} duration={2} delay={0} />
+                    )}
                   </span>
                 </ScrollTrigger>
                 <h5>Children</h5>
               </div>
               <div className={styles.discovery_main_second_third_second}>
-                <ScrollTrigger onEnter={() => setCounterOn(true)} onExit={() => setCounterOn(false)}>
+                <ScrollTrigger
+                  onEnter={() => setCounterOn(true)}
+                  onExit={() => setCounterOn(false)}
+                >
                   <span>
-                    {counteron && (<CountUp start={0} end={38} duration={2} delay={0} /> )}
+                    {counteron && (
+                      <CountUp start={0} end={38} duration={2} delay={0} />
+                    )}
                   </span>
                 </ScrollTrigger>
                 <h5>Orphanage</h5>
@@ -104,6 +125,43 @@ const Home = () => {
           <div className={styles.discovery_main_second_first}>
             <img src="disc 2.webp" alt="correct"></img>
           </div>
+        </div>
+      </div>
+      {/*  */}
+      <div className={styles.major_main}>
+        <div className={styles.major_main_first}>
+          <img src="home1.png.webp" alt="correct"></img>
+          <h2>Total Donation</h2>
+          <p>
+            The French Revolutioncons tituted for the conscience of the
+            dominant.
+          </p>
+        </div>
+        <div className={styles.major_main_first}>
+        <img src="home2.png.webp" alt="correct"></img>
+          <h2>Total Donation</h2>
+          <p>
+            The French Revolutioncons tituted for the conscience of the
+            dominant.
+          </p>
+        </div>
+        <div className={styles.major_main_first}>
+         
+        <img src="home3.png.webp" alt="correct"></img>
+          <h2>Total Donation</h2>
+          <p>
+            The French Revolutioncons tituted for the conscience of the
+            dominant.
+          </p>
+        </div>
+        <div className={styles.major_main_first}>
+          
+        <img src="home4.png.webp" alt="correct"></img>
+          <h2>Total Donation</h2>
+          <p>
+            The French Revolutioncons tituted for the conscience of the
+            dominant.
+          </p>
         </div>
       </div>
     </>
