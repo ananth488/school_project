e456c6eb380b5c87164630d39f30e32afb36fd59
ananth488 <ananthjacks28@gmail.com>
2023-10-28 16:10:06 +0530

    ddsf

--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -1,18 +1,26 @@
-import React from 'react'
-import Styles from './Gallery.module.css'
+import React from "react";
+import Styles from "./Gallery.module.css";
 
 const Gallery = () => {
   return (
     <>
-    {/* gallery banner */}
-    <div className={Styles.gallery_main}>
-      <div className={Styles.gallery_main_head}>
-      <h2>Gallery</h2>
-      <p>We, Thaagam Foundation, a non-government organisation, is working towards elevating the livelihoods of all living beings.</p>
+      {/* gallery banner */}
+      <div className={Styles.gallery_main}>
+        <div className={Styles.gallery_main_head}>
+          <h2>Gallery</h2>
+          <p>
+            We, Thaagam Foundation, a non-government organisation, is working
+            towards elevating the livelihoods of all living beings.
+          </p>
+        </div>
       </div>
-    </div>
+      <div className={Styles.gallery_main_photo}>
+        <div className={Styles.gallery_main_photos}>
+          <img src="CHILD 1.jpg" alt="..."></img>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
