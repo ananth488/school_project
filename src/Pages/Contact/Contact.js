8af8665712f59bc8de915090d969957fcd747c28
ananth488 <ananthjacks28@gmail.com>
2023-10-29 17:50:30 +0530

    ffgh

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Styles from "./Contact.module.css";
+import  * as FaLocationDot from 'react-icons/fa'
 const Contact = () => {
   return (
     <>
@@ -17,9 +18,17 @@ const Contact = () => {
       <div className={Styles.contact_form_main}>
         <div className={Styles.contact_form_main_first}>
           <h2>Quick Contact</h2>
+          <div className={Styles.contact_form_main_first_content}>
+            <div className={Styles.contact_form_main_first_content_f}>
+             <FaLocationDot/>
+            </div>
+            <div className={Styles.contact_form_main_first_content_s}>
+
+            </div>
+          </div>
         </div>
         <div className={Styles.contact_form_main_second}>
-        <h2>Quick Contact Form</h2>
+          <h2>Quick Contact Form</h2>
         </div>
       </div>
     </>
