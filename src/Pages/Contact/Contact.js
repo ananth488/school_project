076e1692c4302f632df9b621359dec0789ae87d5
ananth488 <ananthjacks28@gmail.com>
2023-10-31 13:13:16 +0530

    jjhj

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -6,19 +6,19 @@ import { Formvalidate } from "./Formvalidate";
 
 const Contact = () => {
   // form validate
-    const Createuser=async(e)=>{
-    e.preventDefault()
-    let formData={
-    name:e.target[0].value,
-    number:e.target[1].value,
-    email:e.target[2].value,
-    password:e.target[3].value
-    }
+  const Createuser = async (e) => {
+    e.preventDefault();
+    let formData = {
+      name: e.target[0].value,
+      number: e.target[1].value,
+      email: e.target[2].value,
+      password: e.target[3].value,
+    };
     console.log(formData);
-    
-    const isValid= await Formvalidate.isValid(formData)
-    console.log(isValid)
-    }
+
+    const isValid = await Formvalidate.isValid(formData);
+    console.log(isValid);
+  };
   // end
   return (
     <>
@@ -45,7 +45,8 @@ const Contact = () => {
                 <span className={Styles.content_form_text}>
                   Our Location <br></br>
                   <span className={Styles.content_form_text1}>
-                  No.6, Dhanammal street, Spurtank road, chetpet Chennai 600-031
+                    No.6, Dhanammal street, Spurtank road, chetpet Chennai
+                    600-031
                   </span>
                 </span>
               </p>
@@ -53,10 +54,11 @@ const Contact = () => {
                 <FaMobile
                   className={Styles.contact_form_main_first_content_icon}
                 />
-                <span className={Styles.content_form_text}>Phone
-                <br></br>
+                <span className={Styles.content_form_text}>
+                  Phone
+                  <br></br>
                   <span className={Styles.content_form_text1}>
-                  +91 80691 69691
+                    +91 80691 69691
                   </span>
                 </span>
               </p>
@@ -64,10 +66,11 @@ const Contact = () => {
                 <MdEmail
                   className={Styles.contact_form_main_first_content_icon}
                 />
-                <span className={Styles.content_form_text}>Email
-                <br></br>
+                <span className={Styles.content_form_text}>
+                  Email
+                  <br></br>
                   <span className={Styles.content_form_text1}>
-                  office@thaagam.org
+                    office@thaagam.org
                   </span>
                 </span>
               </p>
@@ -75,21 +78,28 @@ const Contact = () => {
           </div>
         </div>
         <div className={Styles.contact_form_main_second}>
-        <h2>Quick Contact Form</h2>
+          <h2>Quick Contact Form</h2>
           <div className={Styles.contact_form_main_second_first}>
             <form onSubmit={Createuser}>
-             <input type="text" placeholder="Enter your username"></input>
-             <input type="number" placeholder="Enter your number"></input>
-             <input type="email" placeholder="Enter your email"></input>
-             <input type="password" placeholder="Enter your password"></input>
-             <div className={Styles.contact_form_main_second_first_click}>
-             <input className={Styles.contact_form_main_second_first_btn} type="submit" ></input>
-             </div>
-             
-             </form>    
+              <input type="text" placeholder="Enter your username"></input>
+              <input type="number" placeholder="Enter your number"></input>
+              <input type="email" placeholder="Enter your email"></input>
+              <input type="password" placeholder="Enter your password"></input>
+              <div className={Styles.contact_form_main_second_first_click}>
+                {/* <input className={Styles.contact_form_main_second_first_btn} type="submit" ></input> */}
+                <button className={Styles.contact_form_main_second_first_btn}>
+                  Submit
+                </button>
+              </div>
+            </form>
           </div>
-
         </div>
+      </div>
+      {/*  */}
+      <div className={Styles.contact_form_main_map}>
+      <iframe title="first"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3886.420508960694!2d80.24402777579904!3d13.072514787252224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5267a0180785f5%3A0x697a4d6c005381ce!2sThaagam%20Foundation!5e0!3m2!1sen!2sin!4v1698733677617!5m2!1sen!2sin"
+      ></iframe>
       </div>
     </>
   );
