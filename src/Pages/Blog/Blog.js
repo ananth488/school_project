b6c1d20ff6a9b2e60aa90b3ec006ad9e89a6d34e
ananth488 <ananthjacks28@gmail.com>
2023-11-01 19:47:28 +0530

    fsf

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,7 +1,30 @@
 import React from "react";
 import Styles from "./Blog.module.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const Blog = () => {
+  // carousel
+
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   return (
     <>
       {/* blog banner */}
@@ -29,6 +52,14 @@ const Blog = () => {
       </div>
 
       {/* owl */}
+      <div className={Styles.blog_main_carousel}>
+      <Carousel responsive={responsive}>
+        <div>Item 1</div>
+        <div>Item 2</div>
+        <div>Item 3</div>
+        <div>Item 4</div>
+      </Carousel>
+      </div>
     </>
   );
 };
